371c13ac5b0cc7758d54e6d070a0a000f7b100c7
Sean Doyle <sdoylelambda@gmail.com>
2019-04-25 18:10:17 -0400

    No errors, no data showing.

---
+++ friends/src/components/Login.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { connect } from "react-redux";
+// import Loader from "react-loader-spinner";
+
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: "",
+            password: ""
+        }
+    };
+
+    handleChanges = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    login = e => {
+        e.preventDefault();
+        this.props
+            .login(this.state.credentials)
+            // /protected    ???
+            .then(() => this.props.history.push("/protected"))
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.login}>
+                    <input
+                        type="text"
+                        name="username"
+                        value={this.state.credentials.username}
+                        onChange={this.handleChanges}
+                    />
+                       <input
+                        type="password"
+                        name="password"
+                        value={this.state.credentials.password}
+                        onChange={this.handleChanges}
+                    />
+                        <button>
+                            {this.props.isLogginIn ? (
+                                "Loading"
+                            ) : (
+                                "Log In"
+                            )}
+                        </button> 
+                </form>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    console.log(state)
+    return {
+        isLogginIn: state.isLogginIn
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    { login }
+)(Login)
