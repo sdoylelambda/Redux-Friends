9ee90bf62387f4506aeeab8a168c5dfd2be9dd66
Sean Doyle <sdoylelambda@gmail.com>
2019-04-25 17:13:01 -0400

    Switched branches, reducer switch statement.

--- friends/src/reducers/friendsReducer.js
+++ friends/src/reducers/friendsReducer.js
@@ -1,11 +1,32 @@
-// imports
+ import { FETCH_FRIENDS_START, FETCH_FRIENDS_SUCCESS, FETCH_FRIENDS_FAILURE} from '../actions'
 
 const initalState = {
-    friends: []
+    friends: [],
+    error: "",
+    isLoading: false
 }
 
 export const friendsReducer = (state = initalState, action) => {
     switch(action.type) {
-        case 
+        case FETCH_FRIENDS_START:
+        return {
+           ...state,
+            error: "",
+            isLoading: true
+        } 
+        case FETCH_FRIENDS_SUCCESS:
+        return {
+           ...state,
+            error: "",
+            isLoading: false
+        } 
+        case FETCH_FRIENDS_FAILURE:
+        return {
+           ...state,
+            error: "",
+            isLoading: false
+        } 
+        default: 
+        return state;
     }
-}
+};
